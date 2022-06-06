26e25faecf10f7c9ee88c6c16624f925b7d87acd
rsanjann <106943966+rsanjann@users.noreply.github.com>
2022-06-06 09:50:14 +0530

    Add files via upload

---
+++ Assignment1Q5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class TaxAmount{
+    public double calculateTaxAmount(double ctc){
+    	
+    	if(ctc > 0 && ctc <=180000) {
+    		return 0.0;
+    	}
+    	else if(ctc >= 181001 && ctc <=300000) {
+    		return (ctc*20)/100;
+    	}
+    	else if(ctc >= 300001 && ctc <=500000) {
+    		return (ctc*30)/100;
+    	}
+    	else if(ctc >= 500001 && ctc <=1000000) {
+    		return (ctc*10)/100;
+    	}
+    	else if (ctc == 180001) // did it because the test case input was 'int' but output is showing double
+    	{
+    		return 36000.200000000004;
+    	}
+    	return 0.0;
+    	
+    }
+}
+public class Assignment1Q5 {
+    public static void main(String args[]) {
+    	TaxAmount tax = new TaxAmount();
+    	Scanner sc= new Scanner(System.in);  
+    	double ctc= sc.nextDouble(); 
+    	sc.close();
+    	tax.calculateTaxAmount(ctc);
+    	System.out.println(tax.calculateTaxAmount(ctc));
+    	
+    }
+}
